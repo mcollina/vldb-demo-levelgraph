027b50269db57c46f06920cab56482c0ff9e1412
antonio <antonio.maccioni@gmail.com>
2016-09-04 16:38:59 +0530

    Fix header and footer positions

--- views/header.js
+++ views/header.js
@@ -4,10 +4,11 @@ const html = require('choo/html')
 
 function header () {
   return html`
+
     <header class="main-header">
-      <img src="/levelgraph.png" >
-      <h2 class="title">Explore New Delhi using</h2><h2 class="system-name" align="middle">LevelGraph</h2>
-       <img style="vertical-align: baseline;"  src="/VLDB-2016-140x140.jpeg">
+        <img src="/levelgraph.png"  width="175px" height="100px" align="left" style="padding-right: 5em; ">
+        <h2 class="title" style="vertical-align:baseline">Explore New Delhi using</h2><h2 class="system-name" align="middle">LevelGraph</h2>
+        <img src="/VLDB-2016-140x140.jpeg" align="right" width="100px" height="100px" style="padding-left: 6em;" >
 
       <p class="description">
 
